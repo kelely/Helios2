90d0b34d6bf12a1c77c9d191e17aedeaf01b7d34
兜小神 <kelely@vip.qq.com>
2016-08-26 16:35:25 +0800

    增加应用程序启动引擎

---
+++ src/Helios.Core/Infrastructure/DependencyManagement/ContainerManager.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Autofac;
+using Autofac.Core.Lifetime;
+
+namespace Helios.Infrastructure.DependencyManagement
+{
+    /// <summary>
+    /// Container manager
+    /// </summary>
+    public class ContainerManager
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="container">Conainer</param>
+        public ContainerManager(IContainer container)
+        {
+            this.Container = container;
+        }
+
+        /// <summary>
+        /// Gets a container
+        /// </summary>
+        public virtual IContainer Container { get; }
+
+        /// <summary>
+        /// Resolve
+        /// </summary>
+        /// <typeparam name="T">Type</typeparam>
+        /// <param name="key">key</param>
+        /// <param name="scope">Scope; pass null to automatically resolve the current scope</param>
+        /// <returns>Resolved service</returns>
+        public virtual T Resolve<T>(string key = "", ILifetimeScope scope = null) where T : class
+        {
+            if (scope == null)
+                scope = Scope();
+
+            if (string.IsNullOrEmpty(key))
+                return scope.Resolve<T>();
+
+            return scope.ResolveKeyed<T>(key);
+        }
+
+        /// <summary>
+        /// Resolve
+        /// </summary>
+        /// <param name="type">Type</param>
+        /// <param name="scope">Scope; pass null to automatically resolve the current scope</param>
+        /// <returns>Resolved service</returns>
+        public virtual object Resolve(Type type, ILifetimeScope scope = null)
+        {
+            if (scope == null)
+                scope = Scope();
+
+            return scope.Resolve(type);
+        }
+
+        /// <summary>
+        /// Resolve all
+        /// </summary>
+        /// <typeparam name="T">Type</typeparam>
+        /// <param name="key">key</param>
+        /// <param name="scope">Scope; pass null to automatically resolve the current scope</param>
+        /// <returns>Resolved services</returns>
+        public virtual T[] ResolveAll<T>(string key = "", ILifetimeScope scope = null)
+        {
+            if (scope == null)
+                scope = Scope();
+
+            if (string.IsNullOrEmpty(key))
+                return scope.Resolve<IEnumerable<T>>().ToArray();
+
+            return scope.ResolveKeyed<IEnumerable<T>>(key).ToArray();
+        }
+
+        /// <summary>
+        /// Resolve unregistered service
+        /// </summary>
+        /// <typeparam name="T">Type</typeparam>
+        /// <param name="scope">Scope; pass null to automatically resolve the current scope</param>
+        /// <returns>Resolved service</returns>
+        public virtual T ResolveUnregistered<T>(ILifetimeScope scope = null) where T:class
+        {
+            return ResolveUnregistered(typeof(T), scope) as T;
+        }
+
+        /// <summary>
+        /// Resolve unregistered service
+        /// </summary>
+        /// <param name="type">Type</param>
+        /// <param name="scope">Scope; pass null to automatically resolve the current scope</param>
+        /// <returns>Resolved service</returns>
+        public virtual object ResolveUnregistered(Type type, ILifetimeScope scope = null)
+        {
+            if (scope == null)
+                scope = Scope();
+
+            var constructors = type.GetConstructors();
+            foreach (var constructor in constructors)
+            {
+                try
+                {
+                    var parameters = constructor.GetParameters();
+                    var parameterInstances = new List<object>();
+                    foreach (var parameter in parameters)
+                    {
+                        var service = Resolve(parameter.ParameterType, scope);
+                        if (service == null)
+                            throw new HeliosException("Unknown dependency");
+                        parameterInstances.Add(service);
+                    }
+                    return Activator.CreateInstance(type, parameterInstances.ToArray());
+                }
+                catch (HeliosException)
+                {
+
+                }
+            }
+            throw new HeliosException("No constructor  was found that had all the dependencies satisfied.");
+        }
+        
+        /// <summary>
+        /// Try to resolve srevice
+        /// </summary>
+        /// <param name="serviceType">Type</param>
+        /// <param name="scope">Scope; pass null to automatically resolve the current scope</param>
+        /// <param name="instance">Resolved service</param>
+        /// <returns>Value indicating whether service has been successfully resolved</returns>
+        public virtual bool TryResolve(Type serviceType, ILifetimeScope scope, out object instance)
+        {
+            if (scope == null)
+                scope = Scope();
+
+            return scope.TryResolve(serviceType, out instance);
+        }
+
+        /// <summary>
+        /// 检查服务是否已经注册了（可以解决）
+        /// </summary>
+        /// <param name="serviceType">Type</param>
+        /// <param name="scope">Scope; pass null to automatically resolve the current scope</param>
+        /// <returns>Result</returns>
+        public virtual bool IsRegistered(Type serviceType, ILifetimeScope scope = null)
+        {
+            if (scope == null)
+                scope = Scope();
+
+            return scope.IsRegistered(serviceType);
+        }
+
+        /// <summary>
+        /// Resolve optional
+        /// </summary>
+        /// <param name="serviceType">Type</param>
+        /// <param name="scope">Scope; pass null to automatically resolve the current scope</param>
+        /// <returns>Resolved service</returns>
+        public virtual object ResolveOptional(Type serviceType, ILifetimeScope scope = null)
+        {
+            if (scope == null)
+                scope = Scope();
+
+            return scope.ResolveOptional(serviceType);
+        }
+        
+        /// <summary>
+        /// Get current scope
+        /// </summary>
+        /// <returns>Scope</returns>
+        public virtual ILifetimeScope Scope()
+        {
+            return Container.BeginLifetimeScope(MatchingScopeLifetimeTags.RequestLifetimeScopeTag);
+        }
+    }
+}
