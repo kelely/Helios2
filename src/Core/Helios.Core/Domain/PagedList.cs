1a5303305c4600ec0c143093f21a95aca694291a
兜小神 <kelely@vip.qq.com>
2016-08-26 17:36:48 +0800

    增加领域模型抽象类以及分页集合支持类

---
+++ src/Core/Helios.Core/Domain/PagedList.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+
+namespace Helios.Domain
+{
+    /// <summary>
+    /// 支持分页的集合类
+    /// </summary>
+    /// <typeparam name="T">T</typeparam>
+    [DataContract]
+    public class PagedList<T>
+    {
+        internal PagedList(IEnumerable<T> items, int pageIndex, int pageSize, int totalCount)
+        {
+            if (items == null)
+                throw new ArgumentNullException(nameof(items));
+
+            if (pageIndex < 0)
+                throw new ArgumentOutOfRangeException(nameof(pageIndex), "页码必须大于0");
+
+            if (pageSize <= 0)
+                throw new ArgumentOutOfRangeException(nameof(pageSize), "分页大小必须大于0");
+
+            this.PageIndex = pageIndex;
+            this.PageSize = pageSize;
+            this.TotalCount = totalCount;
+
+            this.TotalPages = TotalCount / this.PageSize;
+
+            // 修正页码
+            if (this.TotalCount % this.PageSize > 0)
+                this.TotalPages++;
+
+            this.Items = items.ToArray();
+        }
+
+        /// <summary>
+        /// 当前数据集合在总数据中的页码,从0开始
+        /// </summary>
+        public int PageIndex { get; }
+
+        /// <summary>
+        /// 分页大小,每页包含多少条数据
+        /// </summary>
+        public int PageSize { get; }
+
+        /// <summary>
+        /// 总数据条数
+        /// </summary>
+        public int TotalCount { get; }
+
+        /// <summary>
+        /// 总页数
+        /// </summary>
+        public int TotalPages { get; }
+
+        /// <summary>
+        /// 是否有上一页
+        /// </summary>
+        public bool HasPreviousPage => (PageIndex > 0);
+
+        /// <summary>
+        /// 是否有下一页
+        /// </summary>
+        public bool HasNextPage => (PageIndex + 1 < TotalPages);
+
+        /// <summary>
+        /// 包含当前页数据的数组集合
+        /// </summary>
+        public T[] Items { get; private set; }
+    }
+}
---
+++ src/Core/Helios.Core/Domain/PagedListExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Helios.Domain
+{
+    public static class PagedListExtensions
+    {
+        public static PagedList<T> ToPagedList<T>(this IEnumerable<T> source, int pageIndex, int pageSize)
+        {
+            return source.AsQueryable().ToPagedList(pageIndex, pageSize);
+        }
+
+        public static PagedList<T> ToPagedList<T>(this IQueryable<T> source, int pageIndex, int pageSize)
+        {
+            var total = source.Count();
+            var items = total == 0 ? new List<T>() : source.Skip(pageIndex * pageSize).Take(pageSize).ToList();
+
+            return new PagedList<T>(items, pageIndex, pageSize, total);
+        }
+    }
+}
