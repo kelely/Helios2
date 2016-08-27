c98f71e721a569d3abbc05d6fc608b1713e7d6d5
兜小神 <kelely@vip.qq.com>
2016-08-27 18:03:17 +0800

    增加服务接口的实现

---
+++ src/Services/Common/Helios.Services.Common.Impl/Services/Common/GenericAttributeService.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Common.Logging;
+using Helios.Caching;
+using Helios.Data;
+using Helios.Domain.Common;
+
+namespace Helios.Services.Common
+{
+    /// <summary>
+    /// 领域实体对象的扩展属性服务接口的实现类
+    /// </summary>
+    public partial class GenericAttributeService : IGenericAttributeService
+    {
+        private static readonly ILog Logger = LogManager.GetLogger<GenericAttributeService>();
+
+        #region Constants
+
+        /// <summary>
+        /// 扩展属性缓存的Key
+        /// </summary>
+        /// <remarks>
+        /// {0} : 领域实体对象的主键Id
+        /// {1} : 领域实体对象的类型
+        /// </remarks>
+        private const string GenericattributeKey = "Helios.genericattribute.{0}-{1}";
+
+        /// <summary>
+        /// 当清除缓存时使用此模式清除缓存
+        /// </summary>
+        private const string GenericattributePatternKey = "Helios.genericattribute.";
+        #endregion
+
+        #region Fields
+
+        private readonly IRepository<GenericAttribute> _genericAttributeRepository;
+        private readonly ICacheManager _cacheManager;
+        // TODO:private readonly IEventPublisher _eventPublisher;
+
+        #endregion
+
+        #region Ctor
+        public GenericAttributeService(ICacheManager cacheManager,
+            IRepository<GenericAttribute> genericAttributeRepository/*,
+                    IEventPublisher eventPublisher*/)
+        {
+            this._cacheManager = cacheManager;
+            this._genericAttributeRepository = genericAttributeRepository;
+            //            this._eventPublisher = eventPublisher;
+        }
+
+        #endregion
+
+        #region Utilities
+
+        /// <summary>
+        /// Deletes an attribute
+        /// </summary>
+        /// <param name="attribute">Attribute</param>
+        protected void DeleteAttribute(GenericAttribute attribute)
+        {
+            if (attribute == null)
+                throw new ArgumentNullException(nameof(attribute));
+
+            _genericAttributeRepository.Delete(attribute);
+
+            Logger.Debug($"领域对象[{attribute.EntityGroup}(#{attribute.EntityId})]的扩展属性[{attribute.Key}]被删除");
+
+            //cache
+            _cacheManager.RemoveByPattern(GenericattributePatternKey);
+
+            //event notification
+            //_eventPublisher.EntityDeleted(attribute);
+        }
+
+        /// <summary>
+        /// Inserts an attribute
+        /// </summary>
+        /// <param name="attribute">attribute</param>
+        protected void InsertAttribute(GenericAttribute attribute)
+        {
+            if (attribute == null)
+                throw new ArgumentNullException(nameof(attribute));
+
+            _genericAttributeRepository.Insert(attribute);
+            Logger.Debug($"领域对象[{attribute.EntityGroup}(#{attribute.EntityId})]增加扩展属性[{attribute.Key}]，属性值[{attribute.Value}]");
+            
+            //cache
+            _cacheManager.RemoveByPattern(GenericattributePatternKey);
+
+            //event notification
+            //            _eventPublisher.EntityInserted(attribute);
+        }
+
+        /// <summary>
+        /// Updates the attribute
+        /// </summary>
+        /// <param name="attribute">Attribute</param>
+        protected void UpdateAttribute(GenericAttribute attribute)
+        {
+            if (attribute == null)
+                throw new ArgumentNullException(nameof(attribute));
+
+            var oldAttribute = GetAttributeById(attribute.Id);
+
+            if(oldAttribute.Value != attribute.Value)
+            {
+                oldAttribute.Value = attribute.Value;
+                _genericAttributeRepository.Update(oldAttribute);
+
+                Logger.Debug($"领域对象[{oldAttribute.EntityGroup}(#{oldAttribute.EntityId})]的扩展属性[{oldAttribute.Key}]被修改，新属性值[{oldAttribute.Value}]");
+
+                //cache
+                _cacheManager.RemoveByPattern(GenericattributePatternKey);
+
+                //event notification
+                //            _eventPublisher.EntityUpdated(attribute);
+            }
+        }
+        #endregion
+
+        #region Methods
+
+        public void SaveAttribute(GenericAttribute attribute)
+        {
+            if (attribute == null)
+                return;
+
+            if (attribute.Id > 0)
+            {
+                if (string.IsNullOrWhiteSpace(attribute.Value))
+                    DeleteAttribute(attribute);
+                else
+                    UpdateAttribute(attribute);
+            }
+            else
+            {
+                InsertAttribute(attribute);
+            }
+        }
+
+        public virtual GenericAttribute GetAttributeById(int attributeId)
+        {
+            if (attributeId == 0)
+                return null;
+
+            return _genericAttributeRepository.Table.FirstOrDefault(ga => ga.Id == attributeId);
+        }
+
+        public virtual IList<GenericAttribute> GetAttributesForEntity(int entityId, string entityGroup)
+        {
+            string key = string.Format(GenericattributeKey, entityId, entityGroup);
+
+            return _cacheManager.Get(key, () => {
+                var query = from ga in _genericAttributeRepository.Table
+                            where ga.EntityId == entityId && ga.EntityGroup.Equals(entityGroup, StringComparison.InvariantCultureIgnoreCase)
+                            select ga;
+                return query.ToList();
+            });
+        }
+
+        #endregion
+    }
+}
---
+++ src/Services/Common/Tests.Helios.Services.Common/Services/Common/GenericAttributeServiceTests.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Helios.Caching;
+using Helios.Data;
+using Helios.Domain.Common;
+using Helios.Services.Common;
+using Rhino.Mocks;
+using Rhino.Mocks.Constraints;
+using Xunit;
+
+namespace Tests.Helios.Services.Common
+{
+    public class GenericAttributeServiceTests 
+    {
+        private readonly IRepository<GenericAttribute> _genericAttributeRepository;
+        private readonly IGenericAttributeService _genericAttributeService;
+
+        public GenericAttributeServiceTests()
+        {
+            var attributes = new List<GenericAttribute>
+            {
+                new GenericAttribute { Id = 1, EntityId = 1, EntityGroup = "helios.domain.customers.customer", Key = "FirstName", Value = "Joy" },
+                new GenericAttribute { Id = 2, EntityId = 1, EntityGroup = "helios.domain.customers.customer", Key = "LastName", Value = "Steve" },
+                new GenericAttribute { Id = 3, EntityId = 1, EntityGroup = "helios.domain.customers.customer", Key = "Gender", Value = "True" },
+                new GenericAttribute { Id = 4, EntityId = 1, EntityGroup = "helios.domain.customers.customer", Key = "DateOfBirth", Value = "1999-04-21" },
+
+                new GenericAttribute { Id = 6, EntityId = 2, EntityGroup = "helios.domain.customers.customer", Key = "FirstName", Value = "Yang" },
+                new GenericAttribute { Id = 7, EntityId = 2, EntityGroup = "helios.domain.customers.customer", Key = "LastName", Value = "Qian" },
+                new GenericAttribute { Id = 8, EntityId = 2, EntityGroup = "helios.domain.customers.customer", Key = "Gender", Value = "False" },
+                new GenericAttribute { Id = 9, EntityId = 2, EntityGroup = "helios.domain.customers.customer", Key = "DateOfBirth", Value = "1996-09-16" },
+            };
+            _genericAttributeRepository = MockRepository.GenerateMock<IRepository<GenericAttribute>>();
+            _genericAttributeRepository.Stub(x => x.Table).Return(attributes.AsQueryable());
+            _genericAttributeRepository.Stub(x => x.TableNoTracking).Return(_genericAttributeRepository.Table);
+
+            //_eventPublisher = MockRepository.GenerateMock<IEventPublisher>();
+
+            _genericAttributeService = new GenericAttributeService(new HeliosNullCache(), _genericAttributeRepository);
+        }
+
+        [Fact(DisplayName = "确保可以通过Id获取通用属性实体对象")]
+        public void Can_get_attribute_by_id()
+        {
+            var ga = _genericAttributeService.GetAttributeById(1);
+
+            ga.Should().NotBeNull();
+            ga.Id.Should().Be(1);
+            ga.EntityId.Should().Be(1);
+        }
+
+        [Fact(DisplayName = "确保可以通过实体Id和名称获取实体扩展属性集合")]
+        public void Can_get_attributes_for_entity()
+        {
+            var attributes = _genericAttributeService.GetAttributesForEntity(1, "helios.domain.customers.customer");
+            attributes.Should().NotBeNull().And.HaveCount(4);
+
+            // 验证大小写无关
+            attributes = _genericAttributeService.GetAttributesForEntity(1, "Helios.Domain.Customers.Customer");
+            attributes.Should().NotBeNull().And.HaveCount(4);
+        }
+
+        [Fact(DisplayName = "确保在扩展属性值为空字符串时，会将通用属性数据删除")]
+        public void Can_delete_generic_attribute_with_empty_value()
+        {
+            var ga = new GenericAttribute { Id = 4, EntityId = 1, EntityGroup = "helios.domain.customers.customer", Key = "DateOfBirth", Value = "" };
+            _genericAttributeService.SaveAttribute(ga);
+
+            _genericAttributeRepository.AssertWasCalled(x => x.Delete(ga));
+            //_eventPublisher.AssertWasCalled(x => x.Publish(Arg<EntityDeleted<GenericAttribute>>.Is.Anything));
+        }
+
+        [Fact(DisplayName = "确保在扩展属性值为空对象时，会将通用属性数据删除")]
+        public void Can_delete_generic_attribute_with_null_object()
+        {
+            var ga = new GenericAttribute { Id = 4, EntityId = 1, EntityGroup = "helios.domain.customers.customer", Key = "DateOfBirth", Value = null };
+            _genericAttributeService.SaveAttribute(ga);
+
+            _genericAttributeRepository.AssertWasCalled(x => x.Delete(ga));
+            //_eventPublisher.AssertWasCalled(x => x.Publish(Arg<EntityDeleted<GenericAttribute>>.Is.Anything));
+        }
+
+        [Fact(DisplayName = "确保可以增加通用属性实体")]
+        public void Can_insert_generic_attribute()
+        {
+            var ga = new GenericAttribute { Id = 0, EntityId = 1, EntityGroup = "helios.domain.customers.customer", Key = "DateOfBirth", Value = "2016-09-16" };
+            _genericAttributeService.SaveAttribute(ga);
+
+            _genericAttributeRepository.AssertWasCalled(x => x.Insert(ga));
+            //_eventPublisher.AssertWasCalled(x => x.Publish(Arg<EntityInserted<GenericAttribute>>.Is.Anything));
+        }
+
+        [Fact(DisplayName = "确保可以更新通用属性实体对象")]
+        public void Can_update_generic_attribute()
+        {
+            var ga = new GenericAttribute { Id = 7, EntityId = 2, EntityGroup = "helios.domain.customers.customer", Key = "LastName", Value = "Guang" };
+            _genericAttributeService.SaveAttribute(ga);
+
+            _genericAttributeRepository.AssertWasCalled(x => x.Update(ga));
+            //_eventPublisher.AssertWasCalled(x => x.Publish(Arg<EntityUpdated<GenericAttribute>>.Is.Anything));
+        }
+    }
+}
