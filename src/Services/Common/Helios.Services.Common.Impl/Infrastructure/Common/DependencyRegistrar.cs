ad3d42e5f9fdec6cdf036bcf2b31f8a21f1f46d1
兜小神 <kelely@vip.qq.com>
2016-08-29 09:33:43 +0800

    增加 Services 基础架构，提供自注册功能

---
+++ src/Services/Common/Helios.Services.Common.Impl/Infrastructure/Common/DependencyRegistrar.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using Autofac.Core;
+using Common.Logging;
+using Helios.Caching;
+using Helios.Configuration;
+using Helios.Infrastructure.DependencyManagement;
+using Helios.Services.Common;
+
+namespace Helios.Infrastructure.Common
+{
+    public class DependencyRegistrar : IDependencyRegistrar
+    {
+        private static readonly ILog Logger = LogManager.GetLogger<DependencyRegistrar>();
+
+        public void Register(ContainerBuilder builder, ITypeFinder typeFinder, HeliosConfig config)
+        {
+            Logger.Debug("注册领域对象扩展属性服务");
+
+            builder.RegisterType<GenericAttributeService>().AsImplementedInterfaces()
+                .WithParameter(ResolvedParameter.ForNamed<ICacheManager>("nop_cache_static"))
+                .InstancePerLifetimeScope();
+        }
+
+        public int Order
+        {
+            get { return 10; }
+        }
+    }
+}
---
+++ src/Services/Common/Helios.Services.Common.Impl/Infrastructure/Common/RouteProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Activation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Routing;
+using Autofac.Integration.Wcf;
+using Common.Logging;
+using Helios.Services.Common;
+using Helios.Web.Routes;
+
+namespace Helios.Infrastructure.Common
+{
+    public class RouteProvider : IRouteProvider
+    {
+        private static readonly ILog Logger = LogManager.GetLogger<RouteProvider>();
+
+        public void RegisterRoutes(RouteCollection routes)
+        {
+            Logger.Debug("注册领域对象扩展属性服务访问路由 /generic-attribute-service");
+            routes.Add(new ServiceRoute("generic-attribute-service", new AutofacServiceHostFactory(), typeof(IGenericAttributeService)));
+        }
+
+        public int Priority => 10;
+    }
+}
