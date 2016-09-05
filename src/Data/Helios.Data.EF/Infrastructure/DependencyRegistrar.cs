e51b6ac9c4698dbc301836f367153eb01de9a747
兜小神 <kelely@vip.qq.com>
2016-09-05 10:32:32 +0800

    整理代码结构

--- src/Data/Helios.Data.EF/Infrastructure/DependencyRegistrar.cs
+++ src/Data/Helios.Data.EF/Infrastructure/DependencyRegistrar.cs
@@ -1,11 +1,10 @@
 ﻿using Autofac;
 using Common.Logging;
 using Helios.Configuration;
-using Helios.Data;
 using Helios.Infrastructure;
 using Helios.Infrastructure.DependencyManagement;
 
-namespace Helios.Infrastructure.Data
+namespace Helios.Data.Infrastructure
 {
     public class DependencyRegistrar : IDependencyRegistrar
     {
@@ -15,6 +14,7 @@ namespace Helios.Infrastructure.Data
         {
             Logger.Debug("注册数据持久化仓库 IRepository<>");
 
+            // TODO: 配置文件中读取连接字符串
             builder.Register<IDbContext>(c => new HeliosObjectContext("Database=helios_entity_extensions;Data Source=localhost;User Id=meiyuner;Pwd=Js9zMr36HxJxUU7C;pooling=false;CharSet=utf8mb4;port=3306;Persist Security Info=True;")).InstancePerLifetimeScope();
 
             builder.RegisterGeneric(typeof(EfRepository<>)).As(typeof(IRepository<>)).InstancePerLifetimeScope();
