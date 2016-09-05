b4f13bbaafb3fc27332e69f2a6059ec700335eb5
兜小神 <kelely@vip.qq.com>
2016-09-05 11:38:03 +0800

    增加 IWebHelper 和 IRoutePublisher 的自注册

---
+++ src/Core/Helios.Core/Helios/Infrastructure/DependencyRegistrar.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+using Common.Logging;
+using Helios.Configuration;
+using Helios.Infrastructure.DependencyManagement;
+using Helios.Web;
+using Helios.Web.Routes;
+
+namespace Helios.Infrastructure
+{
+    public class DependencyRegistrar :IDependencyRegistrar
+    {
+        private static readonly ILog Logger = LogManager.GetLogger<DependencyRegistrar>();
+
+        public void Register(ContainerBuilder builder, ITypeFinder typeFinder, HeliosConfig config)
+        {
+            Logger.Debug("注册 Helios 核心接口");
+            builder.RegisterType<WebHelper>().As<IWebHelper>().SingleInstance();
+            builder.RegisterType<RoutePublisher>().As<IRoutePublisher>().SingleInstance();
+        }
+
+        public int Order => -999;
+    }
+}
